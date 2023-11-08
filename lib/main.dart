d8d57db745ac69f4062e45d12d224870cffaf8fa
maikdejong <82510647+maikdejong@users.noreply.github.com>
2023-11-08 13:38:31 +0100

    NavigationWidget gemaakt met placeholder pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,93 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProductsApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ProductsApp extends StatefulWidget {
+  const ProductsApp({super.key});
+
+  @override
+  State<ProductsApp> createState() => _ProductsAppState();
+}
+
+class _ProductsAppState extends State<ProductsApp> {
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter MADkd',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: NavigationWidget(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
+class NavigationWidget extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<NavigationWidget> createState() => _NavigationWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _NavigationWidgetState extends State<NavigationWidget> {
+  var selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Flutter Demo Home Page'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+    Widget widget;
+
+    Map<int, Widget> widgets = {
+      0: Placeholder(),
+      1: Placeholder(),
+      2: Placeholder(),
+      3: Placeholder(),
+    };
+
+    widget = widgets[selectedIndex] ?? Placeholder();
+
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                extended: constraints.maxWidth >= 700,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Home'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.production_quantity_limits),
+                    label: Text('Products'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.contact_page),
+                    label: Text('Contact'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.settings), 
+                    label: Text('Settings')
+                    ),
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  setState(() {
+                    selectedIndex = (value);
+                  });
+                },
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: widget,
+              ),
+            ),
+          ],
         ),
-      ),
-    );
+      );
+    });
   }
 }
